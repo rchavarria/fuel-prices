e2ed10030f79a2bbb2ed27fd8fb64ac0b2f889f0
Rubén Chavarría <txingo@gmail.com>
2019-10-16 16:32:23 +0200

    :alembic: Making the very first request
    As expected, it fails because of wrong CORS request. More investigation is needed

--- src/index.js
+++ src/index.js
@@ -1,1 +1,32 @@
 console.log('Hook index.js')
+
+const targetUrl = 'https://geoportalgasolineras.es/rest/busquedaEstaciones'
+const body = {
+  'tipoEstacion': 'EESS',
+  'idProvincia': '19',
+  'idMunicipio': 20378,
+  'idProducto': 4,
+  'rotulo': '',
+  'eessEconomicas': false,
+  'conPlanesDescuento': false,
+  'horarioInicial': null,
+  'horarioFinal': null,
+  'calle': '',
+  'numero': '',
+  'codPostal': '',
+  'tipoVenta': null,
+  'idOperador': null,
+  'nombrePlan': '',
+  'idTipoDestinatario': null
+}
+
+fetch(targetUrl, {
+  method: 'post',
+  headers: {
+    'Accept': 'application/json, text/plain, */*',
+    'Content-Type': 'application/json'
+  },
+  body: JSON.stringify(body)
+})
+  .then(res => res.json())
+  .then(res => console.log('result:', res))
