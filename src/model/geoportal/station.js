74beb41bf33eddac000bdde8f5ff30801b3fa9a2
Rubén Chavarría <txingo@gmail.com>
2019-10-17 13:25:23 +0200

    :label: Add classes to model data received from geoportalgosolineras.es

---
+++ src/model/geoportal/station.js
@@ -0,0 +1,11 @@
+export default class Station {
+  constructor () {
+    this.id = 0
+    this.rotulo = 'rotulo'
+    this.direccion = 'direccion'
+    this.municipio = 'municipio'
+    this.fechaPvp = new Date()
+    this.favorita = false
+    this.valoracion = 0
+  }
+}
