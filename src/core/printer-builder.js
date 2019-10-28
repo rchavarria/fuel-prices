74133de90d038c594c5e23207d4ab1aecd3c0ead
Rubén Chavarría <txingo@gmail.com>
2019-10-28 22:33:27 +0100

    :white_check_mark: Implement test: logs lines for each station

--- src/core/printer-builder.js
+++ src/core/printer-builder.js
@@ -1,5 +1,9 @@
 export default function (logger) {
   return function (stations) {
     logger.log(`Hay ${stations.length} estaciones`)
+
+    stations.forEach(station => {
+      logger.log(station.price)
+    })
   }
 }
