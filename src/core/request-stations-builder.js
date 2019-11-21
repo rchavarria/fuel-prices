0f77697cb771c14b41fb2606fe80a3a67c49a0d0
Rubén Chavarría <txingo@gmail.com>
2019-11-21 22:31:31 +0100

    :white_check_mark: Implement test: makes a request to a specific URL

--- src/core/request-stations-builder.js
+++ src/core/request-stations-builder.js
@@ -1,8 +1,9 @@
 import StationRecord from '../model/geoportal/station-record'
+import { STATIONS_URL } from './stations-url'
 
 export default function (restClient) {
   return function () {
-    return restClient.post()
+    return restClient.post(STATIONS_URL)
       .then(response => response.data)
       .then(data => data.estaciones.map(StationRecord.fromRequest))
   }
