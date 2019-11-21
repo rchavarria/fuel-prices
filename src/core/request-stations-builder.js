502fa440c84dffbd335a3edc64127cd683cb75ab
Rubén Chavarría <txingo@gmail.com>
2019-11-21 22:24:39 +0100

    :white_check_mark: Implement test: parses the response data to build station records

--- src/core/request-stations-builder.js
+++ src/core/request-stations-builder.js
@@ -1,5 +1,9 @@
+import StationRecord from '../model/geoportal/station-record'
+
 export default function (restClient) {
   return function () {
     return restClient.post()
+      .then(response => response.data)
+      .then(data => data.estaciones.map(StationRecord.fromRequest))
   }
 }
