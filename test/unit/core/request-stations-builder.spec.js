afabe854234fae25b5f1c99a9b2f92fbe5fcd43f
Rubén Chavarría <txingo@gmail.com>
2019-11-21 22:24:26 +0100

    :x: Failing test: parses the response data to build station records

--- test/unit/core/request-stations-builder.spec.js
+++ test/unit/core/request-stations-builder.spec.js
@@ -1,5 +1,6 @@
 /* eslint-env jest */
 import requestStationsBuilder from '../../../src/core/request-stations-builder'
+import StationRecord from '../../../src/model/geoportal/station-record'
 
 describe('requestStationsBuilder', () => {
   let requestStations
@@ -13,12 +14,68 @@ describe('requestStationsBuilder', () => {
   })
 
   it('accepts a rest client as parameter and uses it', () => {
+    const fakeResponse = {
+      data: {
+        estaciones: [
+          {
+            id: null,
+            precio: 1.205,
+            estacion: {
+              id: 11591,
+              rotulo: 'REPSOL',
+              direccion: 'CALLE SETAS, 1',
+              margen: 'D',
+              codPostal: '19162',
+              localidad: 'PIOZ                     ',
+              fechaPvp: '11/11/2019',
+              favorita: false
+            },
+            favorita: false
+          }
+        ]
+      }
+    }
+
     const fakeRestClient = {
-      post: jest.fn()
+      post: jest.fn(() => new Promise(resolve => resolve(fakeResponse)))
     }
 
     requestStations = requestStationsBuilder(fakeRestClient)
     requestStations()
     expect(fakeRestClient.post).toBeCalled()
+  })
+
+  it('parses the response data to build station records', () => {
+    const fakeResponse = {
+      data: {
+        estaciones: [
+          {
+            id: null,
+            precio: 1.205,
+            estacion: {
+              id: 11591,
+              rotulo: 'REPSOL',
+              direccion: 'CALLE SETAS, 1',
+              margen: 'D',
+              codPostal: '19162',
+              localidad: 'PIOZ',
+              fechaPvp: '11/11/2019',
+              favorita: false
+            },
+            favorita: false
+          }
+        ]
+      }
+    }
+
+    const fakeRestClient = {
+      post: jest.fn(() => new Promise(resolve => resolve(fakeResponse)))
+    }
+
+    requestStations = requestStationsBuilder(fakeRestClient)
+    return requestStations()
+      .then(records => {
+        expect(records[0]).toBeInstanceOf(StationRecord)
+      })
   })
 })
