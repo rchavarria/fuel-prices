8e5bb78f389849e8d315f11a42bbf412414141ff
Rubén Chavarría <txingo@gmail.com>
2019-11-24 20:37:54 +0100

    :recycle: Extract method

--- src/model/geoportal/station-mapper.js
+++ src/model/geoportal/station-mapper.js
@@ -1,22 +1,24 @@
 import MyStation from './my-station'
 
+function eachStation (responseStation) {
+  const station = new MyStation()
+
+  station.id = responseStation.estacion.id
+  station.price = responseStation.precio
+  station.label = responseStation.estacion.rotulo
+  station.address = responseStation.estacion.direccion
+  station.city = responseStation.estacion.localidad
+  station.priceDate = responseStation.estacion.fechaPvp
+
+  return station
+}
+
 export default class StationMapper {
   mapAll (responseData) {
     if (!responseData) {
       return []
     }
 
-    return responseData.estaciones.map(responseStation => {
-      const station = new MyStation()
-
-      station.id = responseStation.estacion.id
-      station.price = responseStation.precio
-      station.label = responseStation.estacion.rotulo
-      station.address = responseStation.estacion.direccion
-      station.city = responseStation.estacion.localidad
-      station.priceDate = responseStation.estacion.fechaPvp
-
-      return station
-    })
+    return responseData.estaciones.map(eachStation)
   }
 }
