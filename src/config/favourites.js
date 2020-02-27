f8099918986843d731290483d4fe6a47afd568fd
Rubén Chavarría <txingo@gmail.com>
2020-02-27 13:25:56 +0100

    :pencil2: Rename file to fix typo

--- src/config/favourites.js
+++ src/config/favourites.js
@@ -3,14 +3,14 @@ import FavouriteStation from '../model/favourite-station'
 export default [
   // Alcala
   new FavouriteStation(3079, 'Alcalá: Alcampo'),
-  new FavouriteStation(2929, 'Alcalá: Galp cerca del Alcampo'),
-  new FavouriteStation(3067, 'Alcalá: Galp Villamalea'),
+  new FavouriteStation(2929, 'Alcalá: Galp del Kirón'),
+  new FavouriteStation(3067, 'Alcalá: Galp Mercadona Meco'),
   new FavouriteStation(4698, 'Alcalá: Galp NII'),
-  new FavouriteStation(4697, 'Alcalá: Galp Mercadona Meco'),
+  new FavouriteStation(4697, 'Alcalá: Galp NII'),
   new FavouriteStation(12721, 'Alcalá: Galp rotonda Fiesta'),
 
   // Guada
-  new FavouriteStation(8292, 'Guada: Galp en rotonda de la bici'),
+  new FavouriteStation(8292, 'Guada: Galp rotonda bici'),
 
   // Pioz
   new FavouriteStation(11591, 'Pioz: Repsol Pioz')
