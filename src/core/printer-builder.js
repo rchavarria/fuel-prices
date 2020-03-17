2075eff60f2d8f9b1629f4558ca1e30d374c4dbb
Rubén Chavarría <txingo@gmail.com>
2020-03-17 16:34:26 +0100

    :recycle: Extract functions to render title and each station

--- src/core/printer-builder.js
+++ src/core/printer-builder.js
@@ -5,6 +5,42 @@ const titleTemplate = `Hay {length} estaciones`
 const stationLineTemplate = `{price}€ en la estación "{description}", {id}, {label} en {address}`
 
 /**
+ * Creates the title
+ *
+ * @param stations {[*]}
+ *
+ * @returns {string}
+ */
+function renderTitle (stations) {
+  const titleParams = [
+    new TemplateParam('length', stations.length)
+  ]
+
+  return render(titleTemplate, titleParams)
+}
+
+/**
+ * Creates one line for each station
+ *
+ * @param stations {[*]}
+ *
+ * @returns {[string]}
+ */
+function renderLines (stations) {
+  return stations.map(stationRecord => {
+    const stationLineParams = [
+      new TemplateParam('id', stationRecord.id),
+      new TemplateParam('label', stationRecord.label),
+      new TemplateParam('address', stationRecord.address),
+      new TemplateParam('price', stationRecord.price),
+      new TemplateParam('description', stationRecord.description)
+    ]
+
+    return render(stationLineTemplate, stationLineParams)
+  })
+}
+
+/**
  * Logs output information based on some templates
  *
  * @param logger
@@ -13,23 +49,10 @@ const stationLineTemplate = `{price}€ en la estación "{description}", {id}, {
  */
 export default function (logger) {
   return function (stations) {
-    const titleParams = [
-      new TemplateParam('length', stations.length)
-    ]
+    const title = renderTitle(stations)
+    const lines = renderLines(stations)
 
-    const lines = stations.map(stationRecord => {
-      const stationLineParams = [
-        new TemplateParam('id', stationRecord.id),
-        new TemplateParam('label', stationRecord.label),
-        new TemplateParam('address', stationRecord.address),
-        new TemplateParam('price', stationRecord.price),
-        new TemplateParam('description', stationRecord.description)
-      ]
-
-      return render(stationLineTemplate, stationLineParams)
-    })
-
-    logger.log(render(titleTemplate, titleParams))
-    lines.forEach(line => logger.log(line))
+    const everything = [ title ].concat(lines)
+    everything.forEach(line => logger.log(line))
   }
 }
