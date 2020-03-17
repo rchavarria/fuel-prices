953b279034b99382f5eb0a38ed3cded2157b5ac7
Rubén Chavarría <txingo@gmail.com>
2020-03-17 16:19:08 +0100

    :bulb: Add documentation filtering favourite stations

--- src/core/filter-favourites.js
+++ src/core/filter-favourites.js
@@ -1,3 +1,11 @@
+/**
+ * Given a list of favourite stations and a list of stations, filter those stations
+ * that are present in the favourite list
+ *
+ * @param favourites
+ *
+ * @returns {function([]): []} Filters the given list of stations
+ */
 export default function filterFavourites (favourites) {
   return function (stationRecords) {
     const favouriteIds = favourites.map(f => f.id)
