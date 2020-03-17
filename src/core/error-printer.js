1e72e1d4a0e9c310a3ce3b9a61e4e383458a16e0
Rubén Chavarría <txingo@gmail.com>
2020-03-17 16:17:06 +0100

    :bulb: Add documentation error logger

--- src/core/error-printer.js
+++ src/core/error-printer.js
@@ -1,3 +1,10 @@
+/**
+ * Creates a function that logs errors
+ *
+ * @param logger
+ *
+ * @returns function Logs an error
+ */
 export default function (logger) {
   return function (error) {
     logger.error('Error:', error)
