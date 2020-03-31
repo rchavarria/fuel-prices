2611c24bcb0c35face9d8ae9579aa2b7a8b962d7
Rubén Chavarría <txingo@gmail.com>
2020-03-31 16:08:11 +0200

    :truck: Move config to a template file
    Real config file will be created from this template, but it'll be ignored from the repo

--- src/config/config.js
+++
@@ -1,7 +0,0 @@
-/**
- * Configuration object
- */
-export default {
-  // URL to get stations information
-  STATIONS_URL: 'https://geoportalgasolineras.es/rest/busquedaEstaciones'
-}
---
+++ src/config/config.template.js
@@ -0,0 +1,26 @@
+import CityId from '../requests/city-id'
+import FavouriteStation from '../model/favourite-station'
+
+/**
+ * Template for the configuration object
+ */
+export default {
+  // URL to get stations information
+  STATIONS_URL: 'https://geoportalgasolineras.es/rest/busquedaEstaciones',
+
+  // cities to request data
+  cities: [
+    new CityId('01', 20001),
+    new CityId('02', 19921),
+    new CityId('03', 31223)
+  ],
+
+  // favourite stations
+  favourites: [
+    new FavouriteStation(1234, 'Alcalá: Alcampo'),
+    new FavouriteStation(5678, 'Ciudad: gasolinera'),
+    new FavouriteStation(1245, 'De aquí: y de allá'),
+    new FavouriteStation(7899, '¿Cómo la identificas tú?')
+  ]
+
+}
