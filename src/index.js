209759fab10c6b4dbaf6a8f6c4394eda3122014d
Rubén Chavarría <txingo@gmail.com>
2020-03-31 16:39:59 +0200

    :recycle: Use a unified config object
    Cities and favourite stations are read from a config object

--- src/index.js
+++ src/index.js
@@ -1,18 +1,17 @@
 import axios from 'axios'
 
 import augmentWithFavourites from './core/augment-with-favourites'
-import cities from './config/cities'
+import config from './config/config'
 import errorPrinter from './core/error-printer'
-import favourites from './config/favourites'
 import filterFavourites from './core/filter-favourites'
 import printerBuilder from './core/printer-builder'
 import requestStationsBuilder from './core/request-stations-builder'
 import sortByPrice from './core/sort-by-price'
 
-Promise.all(cities.map(requestStationsBuilder(axios)))
+Promise.all(config.cities.map(requestStationsBuilder(axios)))
   .then(allStations => allStations.flat())
-  .then(filterFavourites(favourites))
-  .then(augmentWithFavourites(favourites))
+  .then(filterFavourites(config.favourites))
+  .then(augmentWithFavourites(config.favourites))
   .then(sortByPrice)
   .then(printerBuilder(console))
   .catch(errorPrinter(console))
