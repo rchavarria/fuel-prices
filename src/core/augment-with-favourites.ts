70dfddfa813c3381977a91d9e7a38d815b357307
Rubén Chavarría <txingo@gmail.com>
2022-06-14 18:39:58 +0200

    :recycle: Migrate method augment with favourites

---
+++ src/core/augment-with-favourites.ts
@@ -0,0 +1,28 @@
+import FavouriteStation from "../model/favourite-station";
+import Station from "../model/geoportal/station";
+
+type Augmenter = (stationRecords: Station[]) => Station[]
+
+/**
+ * Given a list of stations and favourites, it matches them by `id` and replaces station's description
+ * with favourite's label in order to being able to better recognize the station when displaying
+ * the report with the list of stations
+ *
+ * @param favourites
+ *
+ * @returns function Function accepting a list of stations
+ */
+export default function augmentWitFavourites (favourites: FavouriteStation[]): Augmenter {
+  return function (stationRecords: Station[]) {
+    const copy = stationRecords.map(s => Object.assign({}, s))
+
+    favourites.forEach(favourite => {
+      const found = copy.findIndex(station => station.id === favourite.id)
+      if (found >= 0) {
+        copy[found].description = favourite.label
+      }
+    })
+
+    return copy
+  }
+}
