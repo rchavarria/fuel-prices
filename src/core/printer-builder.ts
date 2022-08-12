c312c2cb68926a4b83784c809523f12b2058653c
Rubén Chavarría <txingo@gmail.com>
2022-08-12 11:30:51 +0200

    :recycle: Migrate function printerBuilder

---
+++ src/core/printer-builder.ts
@@ -0,0 +1,60 @@
+import render from './render'
+import TemplateParam from '../model/template-param'
+import Station from '../model/geoportal/station'
+import {ILogger} from './ILogger'
+
+const titleTemplate = `Hay {length} estaciones`
+const stationLineTemplate = `{price}€ en la estación "{description}", {id}, {label} en {address}`
+
+/**
+ * Creates the title
+ *
+ * @param stations {[*]}
+ *
+ * @returns {string}
+ */
+function renderTitle (stations: Station[]) {
+  const titleParams = [
+    new TemplateParam('length', stations.length.toString())
+  ]
+
+  return render(titleTemplate, titleParams)
+}
+
+/**
+ * Creates one line for each station
+ *
+ * @param stations {[*]}
+ *
+ * @returns {[string]}
+ */
+function renderLines (stations: Station[]): string[] {
+  return stations.map((stationRecord: Station): string => {
+    const stationLineParams = [
+      new TemplateParam('id', stationRecord.id.toString()),
+      new TemplateParam('label', stationRecord.label),
+      new TemplateParam('address', stationRecord.address),
+      new TemplateParam('price', stationRecord.price.toFixed(3)),
+      new TemplateParam('description', stationRecord.description)
+    ]
+
+    return render(stationLineTemplate, stationLineParams)
+  })
+}
+
+/**
+ * Logs output information based on some templates
+ *
+ * @param logger
+ *
+ * @returns {function([*])}
+ */
+export default function (logger: ILogger) {
+  return function (stations: Station[]) {
+    const title = renderTitle(stations)
+    const lines = renderLines(stations)
+
+    const everything = [ title, ...lines ]
+    everything.forEach(line => logger.log(line))
+  }
+}
--- src/index-screen.ts
+++ src/index-screen.ts
@@ -4,13 +4,12 @@ import filterFavourites from './core/filter-favourites'
 import requestStationsBuilder from './core/request-stations-builder'
 import augmentWithFavourites from './core/augment-with-favourites'
 import sortByPrice from './core/sort-by-price'
+import printerBuilder from './core/printer-builder'
 
 Promise.all(Config.cities.map(requestStationsBuilder(axios, Config.STATIONS_URL)))
   .then(allStations => allStations.flat())
   .then(filterFavourites(Config.favourites))
   .then(augmentWithFavourites(Config.favourites))
   .then(sortByPrice)
-  .then(stations => {
-    console.log('fav stations', stations)
-    return stations
-  })
+  .then(printerBuilder(console))
+
