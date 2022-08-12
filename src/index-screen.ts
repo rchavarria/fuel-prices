6950f52b76c635b9787313d68b9ccdd8d5e20b50
Rubén Chavarría <txingo@gmail.com>
2022-08-12 11:18:43 +0200

    :sparkles: index script makes requests to geoportal gasolineras

--- src/index-screen.ts
+++ src/index-screen.ts
@@ -1,4 +1,12 @@
+import axios from 'axios'
 import Config from './config/config'
 import filterFavourites from './core/filter-favourites'
+import requestStationsBuilder from './core/request-stations-builder'
 
-console.log('favorites', filterFavourites(Config.favourites)([]))
+Promise.all(Config.cities.map(requestStationsBuilder(axios, Config.STATIONS_URL)))
+  .then(allStations => allStations.flat())
+  .then(filterFavourites(Config.favourites))
+  .then(favs => {
+    console.log('fav stations', favs)
+    return favs
+  })
