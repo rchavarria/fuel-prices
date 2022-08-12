04a2b2cf17e4c32561d2ff98de958cbd83e8207e
Rubén Chavarría <txingo@gmail.com>
2022-08-12 11:21:51 +0200

    :recycle: Migrate function sortByPrice

--- src/index-screen.ts
+++ src/index-screen.ts
@@ -2,11 +2,15 @@ import axios from 'axios'
 import Config from './config/config'
 import filterFavourites from './core/filter-favourites'
 import requestStationsBuilder from './core/request-stations-builder'
+import augmentWithFavourites from './core/augment-with-favourites'
+import sortByPrice from './core/sort-by-price'
 
 Promise.all(Config.cities.map(requestStationsBuilder(axios, Config.STATIONS_URL)))
   .then(allStations => allStations.flat())
   .then(filterFavourites(Config.favourites))
-  .then(favs => {
-    console.log('fav stations', favs)
-    return favs
+  .then(augmentWithFavourites(Config.favourites))
+  .then(sortByPrice)
+  .then(stations => {
+    console.log('fav stations', stations)
+    return stations
   })
