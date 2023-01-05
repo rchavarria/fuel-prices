eb40604c6e23afbfa34d6115789ca58371d92a03
Rubén Chavarría <txingo@gmail.com>
2023-01-05 16:49:45 +0100

    :recycle: keep just one CLI version
    Remove the old version and keep the one using the public API

--- src/index-screen.ts
+++ src/index-screen.ts
@@ -1,13 +1,12 @@
-import axios from 'axios'
-import Config from './config/config'
-import filterFavourites from './core/filter-favourites'
-import requestStationsBuilder from './core/request-stations-builder'
-import augmentWithFavourites from './core/augment-with-favourites'
-import sortByPrice from './core/sort-by-price'
-import printerBuilder from './core/printer-builder'
-import errorPrinter from './core/error-printer'
+import Config from './config/config';
+import augmentWithFavourites from './core/augment-with-favourites';
+import errorPrinter from './core/error-printer';
+import filterFavourites from './core/filter-favourites';
+import printerBuilder from './core/printer-builder';
+import sortByPrice from './core/sort-by-price';
+import {requestStationsOn} from './minetur/adapters/services/request-stations-on';
 
-Promise.all(Config.cities.map(requestStationsBuilder(axios, Config.STATIONS_URL)))
+Promise.all(Config.cities.map(requestStationsOn))
   .then(allStations => allStations.flat())
   .then(filterFavourites(Config.favourites))
   .then(augmentWithFavourites(Config.favourites))
